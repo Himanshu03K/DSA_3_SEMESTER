db03f35b5dcdf6fe128b31024c8b7b0628289dc2
Himanshu Kushwaha <kushwaha0312@gmail.com>
2022-02-13 23:01:52 +0530

    Brackets Balanced

--- src/Stack/Q1.java
+++ src/Stack/Q1.java
@@ -1,4 +1,60 @@
 package Stack;
+/*Brackets Balanced
+
+For a given a string expression containing only round brackets or parentheses, check if they are balanced or not. Brackets are said to be balanced if the bracket which opens last, closes first.
+Example:
+Expression: (()())
+Since all the opening brackets have their corresponding closing brackets, we say it is balanced and hence the output will be, 'true'.
+You need to return a boolean value indicating whether the expression is balanced or not.
+Note:
+The input expression will not contain spaces in between.
+Input Format:
+The first and the only line of input contains a string expression without any spaces in between.
+ Output Format:
+The only line of output prints 'true' or 'false'.
+Note:
+You don't have to print anything explicitly. It has been taken care of. Just implement the function.
+Constraints:
+1 <= N <= 10^7
+ Where N is the length of the expression.
+
+Time Limit: 1sec
+Sample Input 1 :
+(()()())
+Sample Output 1 :
+true
+Sample Input 2 :
+()()(()
+Sample Output 2 :
+false
+Explanation to Sample Input 2:
+The initial two pairs of brackets are balanced. But when you see, the opening bracket at the fourth index doesn't have its corresponding closing bracket which makes it imbalanced and in turn, making the whole expression imbalanced. Hence the output prints 'false'.
+
+ */
+
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Q1 {
+    public static boolean checkBrackets(String s){
+        Stack<Character> stack =new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char a=s.charAt(i);
+            if(a=='('){
+                stack.push(a);
+            }
+            else if(stack.empty())
+                return false;
+            else if(a==')' && stack.pop()!='('){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the String : ");
+        String str=sc.next();
+        System.out.println(checkBrackets(str));
+    }
 }

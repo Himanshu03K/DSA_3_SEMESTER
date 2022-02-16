d601aef357dc7d8056332103fe321c828e7159db
Himanshu Kushwaha <kushwaha0312@gmail.com>
2022-02-16 22:24:13 +0530

    Creating Node Class for LL

---
+++ src/PriorityQueuewithLinkedList/Node.java
@@ -0,0 +1,12 @@
+package PriorityQueuewithLinkedList;
+
+public class Node {
+    Node next;
+    int data;
+    int priority;
+    public Node(int data, int priority)
+    {
+        this.data = data;
+        this.priority = priority;
+    }
+}

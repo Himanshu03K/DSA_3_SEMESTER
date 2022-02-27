0b66b3b3b1f26e1c9e16df2c86027a5abda596fb
Himanshu Kushwaha <kushwaha0312@gmail.com>
2022-02-27 16:30:10 +0530

    TowerOfHanoi

--- src/RECURTION/TowerOfHanoi.java
+++ src/RECURTION/TowerOfHanoi.java
@@ -3,5 +3,19 @@ package RECURTION;
 import java.util.Scanner;
 
 public class TowerOfHanoi {
+    static void tower(int n, char from_rod, char to_rod, char aux_rod){
+        if(n == 0)
+            return;
+        tower(n-1,from_rod,aux_rod,to_rod);
+        System.out.println("Move disk "+ n+ " from rod "+from_rod+ " to rod "+to_rod);
 
+        tower(n-1,aux_rod,to_rod,from_rod);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number of disk");
+        int n= sc.nextInt();
+        tower(n,'A','B','C');
+    }
 }

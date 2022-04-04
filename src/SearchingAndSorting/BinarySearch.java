ca01c9a99263d0dcdb0bd3793d4e796b538e4f95
Himanshu Kushwaha <kushwaha0312@gmail.com>
2022-04-04 22:24:08 +0530

    Binary Search

--- src/SearchingAndSorting/BinarySearch.java
+++ src/SearchingAndSorting/BinarySearch.java
@@ -1,4 +1,25 @@
 package SearchingAndSorting;
 
 public class BinarySearch {
+    public static void BinSearch(int arr[]){
+        int n= arr.length;;
+        int beg=0,end= n-1;
+        if(beg<=end)
+        {
+            int mid=(beg+end)/2;
+            if(n==arr[mid])
+            {
+                System.out.println(arr[mid]+"  -->  Found at index  -->  "+mid);
+                System.exit(0);
+            }
+            if(n>arr[mid])
+                beg=mid+1;
+            if(n<arr[mid])
+                end=mid-1;
+            BinSearch(arr);
+        }
+        else
+            System.out.println("Element not found");
+    }
+
 }

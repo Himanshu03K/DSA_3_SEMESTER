7ef8ea3767cb685a1b1607e67057c89bb34751d6
Himanshu Kushwaha <kushwaha0312@gmail.com>
2022-04-18 23:48:12 +0530

    init

--- src/BinaryTree/Traversals/LevelOrderByQueue.java
+++ src/BinaryTree/Traversals/LevelOrderByQueue.java
@@ -1,30 +1,30 @@
-package BinaryTree.Traversals;
-
-import BinaryTree.BinaryNode;
-import BinaryTree.BinaryTree;
-import Queue.PriorityQueuewithLinkedList.Node;
-
-import java.util.LinkedList;
-import java.util.Queue;
-
-public class LevelOrderByQueue {
-    public static void PrintLevelOrder(BinaryNode root){
-        QueueByLLGeneric<> q= new QueueByLLGeneric<>();
-        q.add(root);
-
-        while (!q.isEmpty()){
-            BinaryNode cur = q.poll();
-            System.out.print(cur.data+" ");
-
-            if (cur.left != null)
-                q.add(cur.left);
-            if(cur.right != null)
-                q.add(cur.right);
-        }
-    }
-
-    public static void main(String[] args) {
-        BinaryNode<Integer> T1= BinaryTree.insert();
-        PrintLevelOrder(T1);
-    }
-}
+//package BinaryTree.Traversals;
+//
+//import BinaryTree.BinaryNode;
+//import BinaryTree.BinaryTree;
+//import Queue.PriorityQueuewithLinkedList.Node;
+//
+//import java.util.LinkedList;
+//import java.util.Queue;
+//
+//public class LevelOrderByQueue {
+//    public static void PrintLevelOrder(BinaryNode root){
+//        QueueByLLGeneric<> q= new QueueByLLGeneric<>();
+//        q.add(root);
+//
+//        while (!q.isEmpty()){
+//            BinaryNode cur = q.poll();
+//            System.out.print(cur.data+" ");
+//
+//            if (cur.left != null)
+//                q.add(cur.left);
+//            if(cur.right != null)
+//                q.add(cur.right);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        BinaryNode<Integer> T1= BinaryTree.insert();
+//        PrintLevelOrder(T1);
+//    }
+//}
